31a319e2220eba1ca743a2833466f3226a10bb0c
harideep123 <70190936+harideep123@users.noreply.github.com>
2023-09-12 13:59:02 +0530

    Add files via upload

---
+++ Task/script.js
@@ -0,0 +1,12 @@
+// JavaScript code goes here
+function toggleDropdown(index) {
+    const dropdown = document.getElementById(`dropdown${index}`);
+    dropdown.classList.toggle('show');
+}
+
+
+
+
+
+
+
